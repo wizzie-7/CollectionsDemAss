87bd2891beaa38f213d81dd72b608613cd2372d9
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:35:19 +0530

    Add project files.

---
+++ BitArrayDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionsDemAss
+{
+    class BitArrayDemo
+    {
+        BitArray BA = new BitArray(10);
+        
+    }
+    class ArrayBit
+    {
+        public void PrintBitArray(IEnumerable IE)
+        {
+            System.Collections.IEnumerator IEN = IE.GetEnumerator();
+            while(IEN.MoveNext())
+            {
+                Console.Write("\t{0}", IEN.Current);
+            }
+        }
+    }
+}
---
+++ CollectionString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace CollectionsDemAss
+{
+    class CollectionString
+    {
+        static void Main(string[] args)
+        {
+            StringCollection SC = new StringCollection();
+            SC.Add("Vivek");
+            SC.Add("Krunal");
+            SC.Add("Nirbhay");
+            SC.Add("Abhi");
+            Console.WriteLine("Number of Items :"+SC.Count);
+            StringCollectionDemo SCD = new StringCollectionDemo();
+            SCD.PrintStrngCollection(SC);
+        }
+    }
+    class StringCollectionDemo
+    {
+        public void PrintStrngCollection(IEnumerable IE)
+        {
+            System.Collections.IEnumerator IEN = IE.GetEnumerator();
+            while (IEN.MoveNext())
+            {
+                Console.WriteLine(IEN.Current);
+            }
+        }
+    }
+}
